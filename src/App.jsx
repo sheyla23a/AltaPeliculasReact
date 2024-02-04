0aaca6d829eb427d44653aed6b30f5673b54697a
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-04 18:20:33 -0300

    agregue el maquetado de mi formulario de peliculas, lo importe en mi app.jsx y le di estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormPeliculas from "./components/FormPeliculas";
+import { Container } from "react-bootstrap";
+
 function App() {
-  return <></>;
+  return <>
+  <section className="text-center mt-4">
+  <h1>Formulario de Peliculas</h1>
+  </section>
+  <Container className="mt-4">
+  <FormPeliculas></FormPeliculas>
+  </Container>
+  </>;
 }
 
 export default App;
---
+++ src/components/FormPeliculas.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const FormPeliculas = () => {
+  const [formData, setFormData] = useState({
+    nombre: "",
+    descripcion: "",
+    genero: "comedia",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Datos enviados:", formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="container mt-4">
+      <div className="mb-3">
+        <label className="form-label">Nombre:</label>
+        <input
+          type="text"
+          name="nombre"
+          value={formData.nombre}
+          onChange={handleChange}
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Descripción:</label>
+        <textarea
+          name="descripcion"
+          value={formData.descripcion}
+          onChange={handleChange}
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label className="form-label">Género:</label>
+        <select
+          name="genero"
+          value={formData.genero}
+          onChange={handleChange}
+          className="form-select"
+        >
+          <option value="comedia">Comedia</option>
+          <option value="drama">Drama</option>
+          <option value="infantil">Infantil</option>
+        </select>
+      </div>
+      <div className="text-center mt-4 ">
+        <button type="submit" className="btn btn-dark">
+          Agregar
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FormPeliculas;
